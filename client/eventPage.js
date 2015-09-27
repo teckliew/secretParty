6bf8d9f354fd94d658a61af3052e2355fd6242b5
teckliew <teck@tekleemedia.com>
2015-09-27 15:46:47 -0700

    editable flow number

--- client/eventPage.js
+++ client/eventPage.js
@@ -1,9 +1,16 @@
 Template.eventPage.events({
-  'keyup #eventphonenum, input': function (evt) {
+  'keyup #eventphonenum': function (evt) {
     var phone = $(evt.target).val();
     if (phone==null){
         phone=0;
     };
     Events.update(this._id, {$set:{"phonenum": phone}});
+  },
+  'keyup #eventflownum': function (evt) {
+    var flow = $(evt.target).val();
+    if (flow==null){
+        flow=0;
+    };
+    Events.update(this._id, {$set:{"flownum": flow}});
   }
 });
