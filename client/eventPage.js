009d5e27ab056d1bf97321be0c691f71fa0a3d1e
teckliew <teck@tekleemedia.com>
2015-09-27 15:43:48 -0700

    editable phone number

--- client/eventPage.js
+++ client/eventPage.js
@@ -1,1 +1,9 @@
-
+Template.eventPage.events({
+  'keyup #eventphonenum, input': function (evt) {
+    var phone = $(evt.target).val();
+    if (phone==null){
+        phone=0;
+    };
+    Events.update(this._id, {$set:{"phonenum": phone}});
+  }
+});
